38177589c19fb1d924f96e08b944d8e323d14d26
Plamen Stoyanov <pazzo_bg@yahoo.com>
2017-05-11 16:22:36 +0300

    100 / 100 Solutions of 04.Lists Lab exercises

---
+++ 04.Lists/04.Lists_Lab/03.SumAdjacentEqualNumbers/SumAdjacentEqualNums.cs
@@ -0,0 +1,26 @@
+﻿namespace _03.SumAdjacentEqualNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SumAdjacentEqualNums
+    {
+        public static void Main()
+        {
+            var inputList = Console.ReadLine().Split(' ').Select(double.Parse).ToList();
+
+            for (int i = 1; i < inputList.Count; i++)
+            {
+                if (inputList[i] == inputList[i - 1])
+                {
+                    inputList[i - 1] *= 2;
+                    inputList.Remove(inputList[i]);
+                    i = 0;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", inputList));
+        }
+    }
+}
